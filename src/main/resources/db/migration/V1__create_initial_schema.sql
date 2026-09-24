27b25d0ada65f86e8760744b4ab625ea05540cd9
Ayush <ayush21052003@gmail.com>
2026-09-25 02:55:59 +0530

    Add initial Flyway database migration

---
+++ src/main/resources/db/migration/V1__create_initial_schema.sql
@@ -0,0 +1,59 @@
+CREATE TABLE users (
+                       userId BIGSERIAL PRIMARY KEY,
+                       name VARCHAR(255) NOT NULL,
+                       email VARCHAR(255) NOT NULL UNIQUE,
+                       password VARCHAR(255) NOT NULL,
+                       role VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE wallets (
+                         walletId BIGSERIAL PRIMARY KEY,
+                         balance NUMERIC(19,2) NOT NULL,
+                         userId BIGINT NOT NULL UNIQUE,
+
+                         CONSTRAINT fk_wallet_user
+                             FOREIGN KEY (userId)
+                                 REFERENCES users(userId)
+);
+
+CREATE TABLE transactions (
+                              transaction_id BIGSERIAL PRIMARY KEY,
+                              sender_wallet_id BIGINT,
+                              receiver_wallet_id BIGINT,
+                              amount NUMERIC(19,2) NOT NULL,
+                              type VARCHAR(50) NOT NULL,
+                              status VARCHAR(50) NOT NULL,
+                              created_at TIMESTAMP NOT NULL,
+
+                              CONSTRAINT fk_transaction_sender_wallet
+                                  FOREIGN KEY (sender_wallet_id)
+                                      REFERENCES wallets(walletId),
+
+                              CONSTRAINT fk_transaction_receiver_wallet
+                                  FOREIGN KEY (receiver_wallet_id)
+                                      REFERENCES wallets(walletId)
+);
+
+CREATE TABLE payments (
+                          payment_id BIGSERIAL PRIMARY KEY,
+                          userId BIGINT NOT NULL,
+                          amount NUMERIC(19,2) NOT NULL,
+                          razorpay_order_id VARCHAR(255) NOT NULL UNIQUE,
+                          razorpay_payment_id VARCHAR(255),
+                          status VARCHAR(50),
+                          created_at TIMESTAMP NOT NULL,
+                          refunded_amount NUMERIC(19,2),
+                          razorpay_refund_id VARCHAR(255),
+
+                          CONSTRAINT fk_payment_user
+                              FOREIGN KEY (userId)
+                                  REFERENCES users(userId)
+);
+
+CREATE TABLE idempotency_keys (
+                                  id BIGSERIAL PRIMARY KEY,
+                                  idempotency_key VARCHAR(255) NOT NULL UNIQUE,
+                                  user_id BIGINT NOT NULL,
+                                  transaction_id BIGINT NOT NULL,
+                                  created_at TIMESTAMP NOT NULL
+);
