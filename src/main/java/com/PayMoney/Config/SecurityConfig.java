4de4205ae96503842c82a688464d2aa6de781177
Ayush <ayush21052003@gmail.com>
2026-09-24 20:13:11 +0530

    Add Actuator monitoring endpoints

--- src/main/java/com/PayMoney/Config/SecurityConfig.java
+++ src/main/java/com/PayMoney/Config/SecurityConfig.java
@@ -38,7 +38,8 @@ public class SecurityConfig {
                                     "/paymoney-logo.png",
                                     "/swagger-ui/**",
                                     "/swagger-ui.html",
-                                    "/v3/api-docs/**"
+                                    "/v3/api-docs/**",
+                                    "/actuator/health"
                             ).permitAll()
                             .anyRequest().authenticated()
                     );
