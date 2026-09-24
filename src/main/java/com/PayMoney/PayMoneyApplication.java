61cecd84a5b95a9892bd671e8adb415ce70de152
Ayush <ayush21052003@gmail.com>
2026-09-24 23:47:47 +0530

    Finalize Docker configuration and application setup

--- src/main/java/com/PayMoney/PayMoneyApplication.java
+++ src/main/java/com/PayMoney/PayMoneyApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-@SpringBootApplication
+import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
+@SpringBootApplication(
+		exclude = UserDetailsServiceAutoConfiguration.class
+)
 public class PayMoneyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PayMoneyApplication.class, args);
 	}
-
 }
